4f433b3f7354a93909af28ce48e0fee26fb4d884
Eduardo Barbosa <ebarbosa.programador@gmail.com>
2024-02-12 15:59:36 -0300

    adicionar funcao para criptografia

--- script.js
+++ script.js
@@ -3,9 +3,36 @@ let textoSaida = document.getElementById('textoSaida');
 let mensagemNaoEncontrada = document.getElementById('mensagemNaoEncontrada');
 
 function criptografar() {
-    let texto = textoEntrada.value;
+    let textoDescriptografado = textoEntrada.value;
+    let textoCriptografado = '';
     
-    mensagemNaoEncontrada.innerHTML = 'criptografado';
+    let strLength = textoDescriptografado.length;
+
+    for (let i = 0; i < strLength; i++) {
+        let letra = textoDescriptografado[i];
+        
+        switch (letra) {
+            case 'a':
+                textoCriptografado += 'ai';
+                break;
+            case 'e':
+                textoCriptografado += 'enter';
+                break;
+            case 'i':
+                textoCriptografado += 'imes';
+                break;
+            case 'o':
+                textoCriptografado += 'ober';
+                break
+            case 'u':
+                textoCriptografado += 'ufat';
+                break
+            default:
+                textoCriptografado += letra;
+        }
+    };
+
+    mensagemNaoEncontrada.innerHTML = textoCriptografado;
 }
 
 function descriptografar() {
